04450c35b70bdb1227935a32612ef1dadef91c45
Owais <owaisstar@gmail.com>
2023-01-31 11:55:16 +0500

    event details components added

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,10 +1,36 @@
+import { useRouter } from 'next/router'
+import { getEventById } from '../../dummy-data'
+import { Fragment } from 'react'
+import EventSummary  from '../../components/event-detail/event-summary'
+import EventLogistics from '../../components/event-detail/event-logistics'
+import EventContent from '../../components/event-detail/event-content'
+
+
 function EventDetailsPage(){
+    const router = useRouter()
+    const eventId = router.query.eventId
+
+    const eventDetails =  getEventById(eventId)
+    if(!eventDetails){
+        return (
+            <p>
+                No Events Found
+            </p>
+        )
+    }
+    const {title, date , location,image,description} =  eventDetails
+
     return (
-        <div>
-            <h2>
-                This is event details page.
-            </h2>
-        </div>
+        <Fragment>
+            <EventSummary title={title} />
+            <EventLogistics date={date} address={location} image={image} imageAlt={title}/>
+            <EventContent>
+                <p>
+                    {description}
+                </p>
+            </EventContent>
+        </Fragment>
+
     )
 }
 
